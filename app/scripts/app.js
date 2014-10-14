6c56fb59a1bd736a34dc336e6c72ddca2557353e
Daniel Egger <daniel.egger@gmail.com>
2014-10-14 17:07:45 +0200

    service

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,47 +14,49 @@ angular.module('destinationApp', ['ngRoute']).config(function($routeProvider) {
         });
 });
 
-angular.module('destinationApp').controller("DestinationController", function() {
+angular.module('destinationApp').factory("DestinationService", function($q) {
+    var destinationData = [{
+        id: "1",
+        name: "Paris",
+        description: "Ein wunderschöner Ort an der Seine",
+        price: 695
+    }, {
+        name: "Interlaken",
+        id: "2",
+        description: "Die Adventure-Hauptstadt im Oberland",
+        price: 899
+    }];
+
+    return {
+        getDestinationData: function() {
+            return $q.when(destinationData);
+        }
+    }
+});
+
+angular.module('destinationApp').controller("DestinationController", function(DestinationService) {
     var self = this;
 
     activate();
 
     function activate() {
-        var destinationData = [{
-            id: "1",
-            name: "Paris",
-            description: "Ein wunderschöner Ort an der Seine",
-            price: 695
-        }, {
-            name: "Interlaken",
-            id: "2",
-            description: "Die Adventure-Hauptstadt im Oberland",
-            price: 899
-        }];
-
-        self.destinationData = destinationData;
+        DestinationService.getDestinationData().then(function(data) {
+            self.destinationData = data;
+        });
     }
 });
 
-angular.module('destinationApp').controller("DestinationDetailController", function($routeParams) {
+angular.module('destinationApp').controller("DestinationDetailController", function($routeParams, DestinationService) {
     var self = this;
 
     activate();
 
     function activate() {
         console.log($routeParams);
-        var destinationData = [{
-            id: "1",
-            name: "Paris",
-            description: "Ein wunderschöner Ort an der Seine",
-            price: 695
-        }, {
-            name: "Interlaken",
-            id: "2",
-            description: "Die Adventure-Hauptstadt im Oberland",
-            price: 899
-        }];
 
-        self.destination = _.findWhere(destinationData, {id: $routeParams.id});
+        DestinationService.getDestinationData().then(function(data) {
+            self.destination = _.findWhere(data, {id: $routeParams.id});
+        });
+
     }
 });
