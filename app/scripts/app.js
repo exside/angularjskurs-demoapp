ea1a74e8e17cbd92f72b1ee0b9336ec65fe678b0
Daniel Egger <daniel.egger@gmail.com>
2014-10-14 16:09:36 +0200

    ng-repeat

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,6 +3,12 @@
 function KursController() {
     this.name = "Daniel";
 
+    this.tasks = [{
+        name: 'Abwaschen', done: false
+    }, {
+        name: 'Putzen', done: true
+    }];
+
     this.sayHello = function() {
         alert(this.name);
     }
