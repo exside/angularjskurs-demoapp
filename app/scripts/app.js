c24b4d0a9f1c099c68061d318eea08c93cbc0da4
Daniel Egger <daniel.egger@gmail.com>
2014-10-14 16:51:59 +0200

    app2 mit routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,60 @@
 'use strict';
+
+angular.module('destinationApp', ['ngRoute']).config(function($routeProvider) {
+    $routeProvider
+        .when('/destination', {
+            templateUrl: 'templates/destination.html',
+            controller: 'DestinationController',
+            controllerAs: 'ctrl'
+        })
+        .when('/destination/:id', {
+            templateUrl: 'templates/destinationdetail.html',
+            controller: 'DestinationDetailController',
+            controllerAs: 'ctrl'
+        });
+});
+
+angular.module('destinationApp').controller("DestinationController", function() {
+    var self = this;
+
+    activate();
+
+    function activate() {
+        var destinationData = [{
+            id: "1",
+            name: "Paris",
+            description: "Ein wunderschöner Ort an der Seine",
+            price: 695
+        }, {
+            name: "Interlaken",
+            id: "2",
+            description: "Die Adventure-Hauptstadt im Oberland",
+            price: 899
+        }];
+
+        self.destinationData = destinationData;
+    }
+});
+
+angular.module('destinationApp').controller("DestinationDetailController", function($routeParams) {
+    var self = this;
+
+    activate();
+
+    function activate() {
+        console.log($routeParams);
+        var destinationData = [{
+            id: "1",
+            name: "Paris",
+            description: "Ein wunderschöner Ort an der Seine",
+            price: 695
+        }, {
+            name: "Interlaken",
+            id: "2",
+            description: "Die Adventure-Hauptstadt im Oberland",
+            price: 899
+        }];
+
+        self.destination = _.findWhere(destinationData, {id: $routeParams.id});
+    }
+});
